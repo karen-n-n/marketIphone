a97b369484e62d3f6bf704b5561660b3f7afa204
karen-n-n <96302109+karen-n-n@users.noreply.github.com>
2021-12-17 18:13:44 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+new Choices('#model', {
+    searchEnabled: false,
+    itemSelectText: '',
+});
+
+new Choices('#color', {
+    searchEnabled: false,
+    itemSelectText: '',
+});
+
+$('#form-order').on('submit', function(event){
+    event.preventDefault();
+
+    $.ajax({
+    url:'https://jsonplaceholder.typicode.com/posts',
+    method: 'POST',
+    dataType: 'html',
+    data: $('#form-order').serialize(),
+    success:function(data){
+        console.log(data);
+        $('#form-order').html('Спасибо! Ваша заявка успешно отправлена!');
+        $('#form__submit').hide();
+    }
+
+    })
+
+})
